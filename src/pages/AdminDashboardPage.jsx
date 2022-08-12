ec3d4a6b3248b9cab38bbb54e849cfb4fc4b1d25
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-08-12 22:01:13 +0100

    start implementation for handleLogOut function

--- src/pages/AdminDashboardPage.jsx
+++ src/pages/AdminDashboardPage.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { AuthContext } from "../authContext";
+import { useNavigate } from "react-router";
 
 const AdminDashboardPage = () => {
+
+  const { dispatch } = React.useContext(AuthContext);
+
+  // handle logout
+
   return (
     <>
       <div className="p-8 m-0 w-full flex flex-col justify-center items-center text-7xl h-screen text-gray-700 bg-black">
         <div className="flex justify-between w-full">
           <span className="text-6xl font-extrabold text-white">APP</span>
-          <button className="rounded-3xl px-7 py-4 text-2xl bg-green-500">Logout</button>
+          <button onClick={()=>logOut()} className="rounded-3xl px-7 py-4 text-2xl bg-green-500">Logout</button>
         </div>
         <div className="h-full w-full border-3">
 
